93b3fd04a887471e3fe3825611e77cc243bd547e
Máique Anderson <maique.ss@gmail.com>
2023-09-19 17:42:57 -0300

    s

--- database/firebaseConfig.js
+++ database/firebaseConfig.js
@@ -8,12 +8,7 @@ import "firebase/firestore";
 
 const firebaseConfig = {
    
-  apiKey: "AIzaSyAzi1-G607OXoMWLtpIEr6qqhX-UK9zgKE",
-  authDomain: "imagigram-10450.firebaseapp.com",
-  projectId: "imagigram-10450",
-  storageBucket: "imagigram-10450.appspot.com",
-  messagingSenderId: "953031958132",
-  appId: "1:953031958132:web:9ae34ab0fd2e6b173cbb21"
+  
   };
 
 export const app = initializeApp(firebaseConfig);
